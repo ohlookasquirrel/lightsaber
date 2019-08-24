f79eb3e6c356858eb71de434c2e79d21e8cb35a7
Michael Hall <mhall1473@gmail.com>
2019-08-23 20:03:38 -0400

    Refactoring mocks, taking out some duplication

--- test_state_manager.py
+++ test_state_manager.py
@@ -19,42 +19,47 @@ from state import State
 import state_manager
 
 
-#  TODO Probably doing some serious overkill with the number of remocking
+@pytest.fixture()
+def hardware():
+    hardware = Hardware(30)
+    hardware.powerButton = MagicMock()
+    hardware.powerButton.pressed.return_value = False
+    type(hardware.accelerometer).acceleration = PropertyMock(return_value=(1, 1, 1))
+    hardware.speaker = MagicMock()
+    hardware.speaker.audio = MagicMock()
+    return hardware
+
+
+def setup_function():
+    state_manager.actions = MagicMock()
+    state_manager.seconds_button_was_pressed = lambda x: 0
+
+
 #  TODO move this to main test
 def test_state_initializes_with_a_mode_of_off():
     state = State()
     assert(state.mode == mode.OFF)
 
 
-def test_get_action_returns_no_op_when_button_not_pressed_and_is_off():
+def test_get_action_returns_no_op_when_button_not_pressed_and_is_off(hardware):
     state = State(mode.OFF)
-    hardware = MagicMock()
-    hardware.powerButton = MagicMock()
-    hardware.powerButton.pressed.return_value = False
-    type(hardware.accelerometer).acceleration = PropertyMock(return_value=(1, 1, 1))
 
     returned_state = state_manager.get_action(state, hardware)
 
     assert returned_state == state
 
 
-def test_get_action_returns_no_op_when_button_not_pressed_and_is_on():
+def test_get_action_returns_no_op_when_button_not_pressed_and_is_on(hardware):
     state = State(mode.ON)
-    hardware = MagicMock()
-    hardware.powerButton = MagicMock()
-    hardware.powerButton.pressed.return_value = False
-    type(hardware.accelerometer).acceleration = PropertyMock(return_value=(1, 1, 1))
 
     returned_state = state_manager.get_action(state, hardware)
 
     assert returned_state == state
 
 
-def test_get_action_calls_power_on_action_when_button_released_and_is_off():
+def test_get_action_calls_power_on_action_when_button_released_and_is_off(hardware):
     state = State(initial_mode=mode.OFF, initial_color=colors.GREEN)
-    hardware = MagicMock()
     state_manager.seconds_button_was_pressed = lambda x: 0.123
-    state_manager.actions = MagicMock()
 
     returned_state = state_manager.get_action(state, hardware)
 
@@ -63,10 +68,8 @@ def test_get_action_calls_power_on_action_when_button_released_and_is_off():
     state_manager.actions.power_on.assert_called_with(hardware, state)
 
 
-def test_get_action_calls_power_off_action_when_button_released_and_is_on():
+def test_get_action_calls_power_off_action_when_button_released_and_is_on(hardware):
     state = State(initial_mode=mode.ON, initial_color=colors.PURPLE)
-    hardware = MagicMock()
-    state_manager.actions = MagicMock()
     state_manager.seconds_button_was_pressed = lambda x: 0.123
 
     returned_state = state_manager.get_action(state, hardware)
@@ -76,36 +79,28 @@ def test_get_action_calls_power_off_action_when_button_released_and_is_on():
     state_manager.actions.power_off.assert_called_with(hardware, state)
 
 
-def test_get_action_plays_idle_sound_when_mode_is_on_and_nothing_else_happens():
+def test_get_action_plays_idle_sound_when_mode_is_on_and_nothing_else_happens(hardware):
     state = State(initial_mode=mode.ON, initial_color=colors.ORANGE)
-    hardware = MagicMock()
-    speaker_mock = MagicMock()
-    speaker_mock.audio = MagicMock()
-    type(speaker_mock.audio).playing = PropertyMock(return_value=False)
-    hardware.speaker = speaker_mock
-    state_manager.sound = MagicMock()
-    state_manager.seconds_button_was_pressed = lambda x: 0
-    hardware.accelerometer = MagicMock()
 
-    type(hardware.accelerometer).acceleration = PropertyMock(return_value=(1, 1, 1))
+    type(hardware.speaker.audio).playing = PropertyMock(return_value=False)
+
+    state_manager.sound = MagicMock()
 
     returned_state = state_manager.get_action(state, hardware)
 
     assert returned_state == state
     state_manager.sound.play_wav.assert_called_with(state.sounds.idle(), hardware.speaker)
 
+    # Teardown
+    import sound as sound_module
+    state_manager.sound = sound_module
 
-def test_get_action_calls_clash_when_accelerometer_crosses_clash_threshold():
+
+def test_get_action_calls_clash_when_accelerometer_crosses_clash_threshold(hardware):
     state = State(initial_mode=mode.ON, initial_color=colors.PINK)
-    hardware = MagicMock()
-    hardware.accelerometer = MagicMock()
-
-    state_manager.actions = MagicMock()
 
     state_manager.CLASH_THRESHOLD = 450
     type(hardware.accelerometer).acceleration = PropertyMock(return_value=(15, 0, 15))
-
-    state_manager.seconds_button_was_pressed = lambda x: 0
 
     returned_state = state_manager.get_action(state, hardware)
 
@@ -114,17 +109,12 @@ def test_get_action_calls_clash_when_accelerometer_crosses_clash_threshold():
     state_manager.actions.clash.assert_called_with(hardware, state)
 
 
-def test_get_action_calls_swing_when_accelerometer_crosses_swing_threshold():
+def test_get_action_calls_swing_when_accelerometer_crosses_swing_threshold(hardware):
     state = State(initial_mode=mode.ON, initial_color=colors.ORANGE)
-    hardware = MagicMock()
-    hardware.accelerometer = MagicMock()
-
-    state_manager.actions = MagicMock()
 
     state_manager.CLASH_THRESHOLD = 600
     state_manager.SWING_THRESHOLD = 200
     type(hardware.accelerometer).acceleration = PropertyMock(return_value=(10, 654, 10))
-    state_manager.seconds_button_was_pressed = lambda x: 0
 
     returned_state = state_manager.get_action(state, hardware)
 
@@ -132,14 +122,10 @@ def test_get_action_calls_swing_when_accelerometer_crosses_swing_threshold():
     state_manager.actions.swing.assert_called_with(hardware, state)
 
 
-def test_get_action_calls_mode_select_when_button_held_for_four_seconds():
+def test_get_action_calls_mode_select_when_button_held_for_four_seconds(hardware):
     state = State(initial_mode=mode.ON, initial_color=colors.CYAN)
-    hardware = MagicMock()
-    hardware.accelerometer = MagicMock()
 
-    type(hardware.accelerometer).acceleration = PropertyMock(return_value=(1, 1, 1))
     state_manager.seconds_button_was_pressed = lambda x: 4
-    state_manager.actions = MagicMock()
 
     expected_selected_mode = mode.SelectableModesItr().current()
 
@@ -151,13 +137,9 @@ def test_get_action_calls_mode_select_when_button_held_for_four_seconds():
     state_manager.actions.mode_select.assert_called_with(hardware, state)
 
 
-def test_get_action_selects_next_mode():
+def test_get_action_selects_next_mode(hardware):
     state = State(initial_mode=mode.MODE_SELECT, initial_color=colors.GREEN)
-    state_manager.actions = MagicMock()
-    hardware = MagicMock()
-    hardware.accelerometer = MagicMock()
 
-    type(hardware.accelerometer).acceleration = PropertyMock(return_value=(1, 1, 1))
     state_manager.seconds_button_was_pressed = lambda x: 1
 
     returned_state = state_manager.get_action(state, hardware)
@@ -170,14 +152,10 @@ def test_get_action_selects_next_mode():
     state_manager.actions.mode_select.assert_called_with(hardware, state)
 
 
-def test_get_action_activates_lightsaber_mode():
+def test_get_action_activates_lightsaber_mode(hardware):
     state = State(initial_mode=mode.MODE_SELECT, initial_color=colors.GREEN)
-    state_manager.actions = MagicMock()
-    hardware = MagicMock()
-    hardware.accelerometer = MagicMock()
-
-    type(hardware.accelerometer).acceleration = PropertyMock(return_value=(1, 1, 1))
     state_manager.seconds_button_was_pressed = lambda x: 4
+
     returned_state = state_manager.get_action(state, hardware)
 
     assert returned_state.color == colors.GREEN
@@ -185,14 +163,10 @@ def test_get_action_activates_lightsaber_mode():
     state_manager.actions.power_on.assert_called_with(hardware, state)
 
 
-def test_get_action_activates_color_change_mode():
+def test_get_action_activates_color_change_mode(hardware):
     state = State(initial_mode=mode.MODE_SELECT, initial_color=colors.GREEN)
     state.mode_selector.next()  # select color change mode TODO improve this
-    state_manager.actions = MagicMock()
-    hardware = MagicMock()
-    hardware.accelerometer = MagicMock()
 
-    type(hardware.accelerometer).acceleration = PropertyMock(return_value=(1, 1, 1))
     state_manager.seconds_button_was_pressed = lambda x: 4
 
     returned_state = state_manager.get_action(state, hardware)
@@ -202,13 +176,10 @@ def test_get_action_activates_color_change_mode():
     state_manager.actions.activate_color_change_mode(hardware)
 
 
-def test_get_action_button_press_changes_to_next_color_in_color_change_mode():
+def test_get_action_button_press_changes_to_next_color_in_color_change_mode(hardware):
     state = State(initial_mode=mode.COLOR_CHANGE, initial_color=colors.ALL_COLORS[0])
-    hardware = MagicMock()
-    hardware.accelerometer = MagicMock()
-    type(hardware.accelerometer).acceleration = PropertyMock(return_value=(1, 1, 1))
+
     state_manager.seconds_button_was_pressed = lambda x: 1
-    state_manager.actions = MagicMock()
 
     returned_state = state_manager.get_action(state, hardware)
 
@@ -217,12 +188,9 @@ def test_get_action_button_press_changes_to_next_color_in_color_change_mode():
     state_manager.actions.next_color.assert_called_with(hardware, state)
 
 
-def test_get_action_powers_on_with_current_color_when_button_held_in_color_change_mode():
+def test_get_action_powers_on_with_current_color_when_button_held_in_color_change_mode(hardware):
     state = State(initial_mode=mode.COLOR_CHANGE, initial_color=colors.NAVY)
-    hardware = MagicMock()
-    state_manager.actions = MagicMock()
-    hardware.accelerometer = MagicMock()
-    type(hardware.accelerometer).acceleration = PropertyMock(return_value=(1, 1, 1))
+
     state_manager.seconds_button_was_pressed = lambda x: 4
 
     returned_state = state_manager.get_action(state, hardware)
@@ -232,21 +200,11 @@ def test_get_action_powers_on_with_current_color_when_button_held_in_color_chang
     state_manager.actions.power_on.assert_called_with(hardware, state)
 
 
-def test_execute_action_activate_selected_mode_activates_wow_mode():
-    import sound
+def test_execute_action_activate_selected_mode_activates_wow_mode(hardware):
     state = State(initial_mode=mode.MODE_SELECT, initial_color=colors.NAVY)
     state.mode_selector.next()
     state.mode_selector.next()  # Select third mode aka wow mode, TODO this needs improving
 
-    hardware = MagicMock()
-    speaker_mock = MagicMock()
-    hardware.speaker = speaker_mock
-
-    state_manager.actions = MagicMock()
-    state_manager.saber = MagicMock()
-    state_manager.sound = sound
-    hardware.accelerometer = MagicMock()
-    type(hardware.accelerometer).acceleration = PropertyMock(return_value=(1, 1, 1))
     state_manager.seconds_button_was_pressed = lambda x: 4
 
     returned_state = state_manager.get_action(state, hardware)
